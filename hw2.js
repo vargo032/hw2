8ecfc2d3ee975d437ffdbd52f91dd6d8aef9e803
vargo032 <o-vargas@kellogg.northwestern.edu>
2016-01-17 22:51:31 -0600

    Added JS form
    adding JS to get subscription buttons to appear inline form.

---
+++ hw2.js
@@ -0,0 +1,10 @@
+function showForm(form) {
+
+    var e = document.getElementById(form);
+
+    if (e.style.display == 'none')
+        e.style.display == 'inline';
+    else
+    e.style.display == 'none';
+
+}
